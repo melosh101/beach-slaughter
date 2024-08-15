80fc53924ed26fe1ea56d46e89e5cf6f5eee80fb
RTS <melosh@melosh.tech>
2024-08-15 10:16:50 +0200

    a start

---
+++ src/_components/nabar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <div className="flex flex-row gap-6 justify-center w-full min-h-14 bg-blue-800 bg-gradient-to-b from-blue-800 from-70% to-slate-900">
+                <Link href="Home" className="hover:bg-blue-700 h-fit rounded p-2">Home</Link>   
+                <Link href="Home" className="hover:bg-blue-700 h-fit rounded p-2">Discography</Link>
+                <Link href="Home" className="hover:bg-blue-700 h-fit rounded p-2">Merch</Link>
+                <Link href="Home" className="hover:bg-blue-700 h-fit rounded p-2 ">Shows</Link>
+            </div>
+        </nav>
+    )
+}
