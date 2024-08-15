18ae38b3ff198c853aabb39609bf7da924a4ecb3
RTS <melosh@melosh.tech>
2024-08-15 11:23:13 +0200

    add speedInsights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Navbar } from "~/_components/nabar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function HomePage() {
   return (
     <main className="w-full min-h-screen bg-slate-900 text-white">
+      <SpeedInsights/>
       <Navbar/>
       <div>
         <Image alt="fælles" src={"/fælles.jpg"} width={"10"} height={"10"} className="aspect-auto w-44"></Image>
