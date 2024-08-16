d25e03ac495f4ecb03c1001b749d4ff2f439b701
melosh <59763532+melosh101@users.noreply.github.com>
2024-08-16 10:57:54 +0200

    Update nabar.tsx
    remove merch link

--- src/_components/nabar.tsx
+++ src/_components/nabar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
             <div className="flex flex-row gap-6 justify-center w-full min-h-14 bg-blue-800 bg-gradient-to-b from-blue-800 from-70% to-slate-900">
                 <Link href="/" className="hover:bg-blue-700 h-fit rounded p-2">Home</Link>   
                 <Link href="#disc" className="hover:bg-blue-700 h-fit rounded p-2">Discography</Link>
-                <Link href="#merch" className="hover:bg-blue-700 h-fit rounded p-2">Merch</Link>
+                
                 <Link href="#shows" className="hover:bg-blue-700 h-fit rounded p-2 ">Shows</Link>
             </div>
         </nav>
